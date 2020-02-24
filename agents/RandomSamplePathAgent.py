b1e1b53072ab0e21e6c365dd7681784b37a1a735
Arturas Aleksandrauskas <Arturas.Aleksandrauskas@contextvision.se>
2020-02-24 08:46:51 +0100

    init random sample aget

---
+++ agents/RandomSamplePathAgent.py
@@ -0,0 +1,15 @@
+class path():
+    def __init__(self):
+        self.waypoints = []
+        self.cost = 0
+        self.reward = 0
+
+
+class RandomSamplePathAgent():
+    def __init__(self):
+        self.path = path()
+
+    def valid_path(self):
+        pass
+
+    
